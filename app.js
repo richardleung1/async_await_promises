b7ad28effe8bdc52b73398e34e9097168b2ac279
Richard Leung <michelleli@MMac.local>
2020-10-29 17:08:39 -0400

    [app.js] Add async functions, await, and promises

--- app.js
+++ app.js
@@ -2,10 +2,26 @@
 // Uncomment these lines and run them, observing the order that the logs happen
 // How can you change it to log in the correct order?
 
+// async function exercise1() {
+//     console.log('I should log before the data!');
+//     const res = await fetch('http://www.reddit.com/search.json?q=cats+nsfw:no')
+//     const json =await res.json()
+//     console.log(json)
+//     console.log('I should log after the data!');
+// }
+// exercise1();
+
+
 // console.log('I should log before the data!');
 // fetch('http://www.reddit.com/search.json?q=cats+nsfw:no')
-// .then((res) => res.json()).then((json) => console.log(json))
-// console.log('I should log after the data!');
+// .then((res) => res.json()).then((json) => {
+//     console.log(json);
+//     console.log('I should log after the data!');
+// });
+
+
+
+
 
 // ========================================
 
@@ -13,8 +29,83 @@
 
 // console.log('I should be before the cats data');
 // fetch('http://www.reddit.com/search.json?q=cats+nsfw:no')
-// .then((res) => res.json()).then((json) => console.log(json))
-// console.log('I should be between the cats and dogs');
-// fetch('http://www.reddit.com/search.json?q=dogs+nsfw:no')
-// .then((res) => res.json()).then((json) => console.log(json))
-// console.log('I should be last');
+// .then((res) => res.json()).then((json) => {
+//     console.log(json);
+//     console.log('I should be between the cats and dogs');
+//     fetch('http://www.reddit.com/search.json?q=dogs+nsfw:no')
+//     .then((res) => res.json()).then((json) => {
+//         console.log(json);
+//         console.log('I should be last');
+//     });
+// });
+
+// async function exercise2() {
+//     console.log('I should be before the cats data');
+
+//     const catRes = await fetch('http://www.reddit.com/search.json?q=cats+nsfw:no');
+//     const catData = await catRes.json();
+//     console.log(catData);
+
+//     console.log('I should be between the cats and dogs');
+
+//     const dogRes = await fetch('http://www.reddit.com/search.json?q=dogs+nsfw:no');
+//     const dogData = await dogRes.json();
+//     console.log(dogData);
+
+//     console.log('I should be last');
+// }
+// exercise2();
+
+// const sucessfulPromise = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//        resolve()
+//       }, 3000)
+// });
+// console.log(sucessfulPromise);
+
+// const unsucessfulPromise =  new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//         reject()
+//     }, 3000)
+// });
+// console.log(unsucessfulPromise);
+// setTimeout(function() {
+//     console.log(sucessfulPromise);
+//     console.log(unsucessfulPromise);
+// }, 4000);
+
+// const conditionalPromise = (condition) => new Promise(function(resolve, reject) {
+//     if (condition) {
+//         setTimeout(function() {resolve()}, 1000)
+//     } else {
+//         setTimeout(function() {reject()}, 1000)
+//     }
+// })
+
+// console.log(conditionalPromise(true));
+
+// function myFetch(url) {
+//     const myPromise = (condition) => new Promise(function(resolve, reject) {
+//         const webData = somehowGetWebData()
+//         if (webData.statusCode < 400) {
+//             resolve();
+//         } else if (400 <=webData.statusCode <= 599) {
+//             reject();
+//         }
+//     })
+
+//     return myPromise;
+// }
+
+// myFetch('some-url.com').then(...).catch(...)
+
+const conditionalPromise = (condition) => new Promise(function(resolve, reject) {
+    if (condition) {
+        resolve();
+    } else {
+        reject();
+    }
+})
+
+console.log(conditionalPromise(true));
+conditionalPromise.then((res) => console.log(res)).catch((err) => console.error (err))
